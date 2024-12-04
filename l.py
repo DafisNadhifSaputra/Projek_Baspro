490e68cd6ea84fdd8dc3146053b2bc652a631122
dafisnadhifsaputra <dafisnadhifs123@gmail.com>
2024-12-04 10:49:49 +0700

    l.py

--- l.py
+++ l.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from PIL import Image, ImageTk
 import os
 
-print("hello00")
+print("hello000")
 class LoginWindow:
     def __init__(self):
         self.window = tk.Tk()
